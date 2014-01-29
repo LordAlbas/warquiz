0d0c73d6bc6ef672ee0918ec09534c1d51a31459
meroveecharrue@gmail.com <meroveecharrue@gmail.com@b4cca557-fefa-2db8-3ffa-aea4168dc914>
2014-01-29 23:39:42 +0000

    Centralisation des images dans une classe specifique

--- trunk/Projet_QUIZ/src/Jouer.java
+++ trunk/Projet_QUIZ/src/Jouer.java
@@ -1,50 +1,32 @@
 import java.awt.Graphics;
-import java.awt.Image;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
-import java.io.File;
-import java.io.IOException;
 
-import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 
 public class Jouer extends JPanel implements MouseListener, MouseMotionListener{
 
-	private Image[] img_fond = new Image[20];			// regroupe les images de fonds
-	private Image[] img_element = new Image[20];		// regroupe les images qui forment les fenetre (barre du haut etc.)
 	private Fenetre fenetre;
+	
 	/**
 	 * Constructeur
 	 */
 	public Jouer(Fenetre fen) {
-		initImage();	// initialisation des images
-		fenetre = fen;  // on récupère la classe mère
-	}	
+		fenetre = fen;  // on rï¿½cupï¿½re la classe mï¿½re
+	}
 	
 	/**
-	 * Initialise les banques d'images a partir des fichiers .png
+	 * Methodes override du MouseListener
 	 */
-	public void initImage() {
-		/*
-		 * charge les images de fonds dans img_fond[]
-		 */
-		try {
-			img_fond[0] = ImageIO.read(new File("images/accueil_fond.png"));	// Lecture du fichier .png
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		/*
-		 * charge les images d'elements dans img_element[]
-		 */
-		try {
-			img_element[0] = ImageIO.read(new File("images/header.png"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}	
+	public void mouseClicked(MouseEvent e) {}
+	public void mouseEntered(MouseEvent e) {}
+	public void mouseExited(MouseEvent e) {}
+	public void mousePressed(MouseEvent e) {}
+	public void mouseReleased(MouseEvent e) {}
+	public void mouseDragged(MouseEvent e) {}
+	public void mouseMoved(MouseEvent e) {}
 	
 	/**
 	 * Paint
@@ -53,29 +35,9 @@ public class Jouer extends JPanel implements MouseListener, MouseMotionListener{
 		//super.paintComponents(g);
 		
 		// le fond et les elements sont en fonction de la taille de la fenetre, donc pas de soucis de redimensionnement de la fenetre
-		g.drawImage(img_fond[0], 0, 0, this.getWidth(), this.getHeight(), null);						// dessine le fond d'ecran
-		g.drawImage(img_element[0], 0, 0, this.getWidth(), (int)(this.getHeight() / 6.1230), null);		// dessine le header
-		
-	}		
-	
-	
-	public void mouseClicked(MouseEvent e) {}
-	public void mouseEntered(MouseEvent e) {}
-	public void mouseExited(MouseEvent e) {}
-	public void mousePressed(MouseEvent e) {}
-	public void mouseReleased(MouseEvent e) {}
-
-	@Override
-	public void mouseDragged(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseMoved(MouseEvent e) {
-		// TODO Auto-generated method stub
+		g.drawImage(Images.img_fond[0], 0, 0, this.getWidth(), this.getHeight(), null);						// dessine le fond d'ecran
+		g.drawImage(Images.img_element[0], 0, 0, this.getWidth(), (int)(this.getHeight() / 6.1230), null);		// dessine le header
 		
 	}
 	
-
 }
