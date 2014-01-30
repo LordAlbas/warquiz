bbd01fb673085ce3c9dd4e3561a82d974d96cadb
mickael.sinardet@gmail.com <mickael.sinardet@gmail.com@b4cca557-fefa-2db8-3ffa-aea4168dc914>
2014-01-30 17:06:48 +0000

    Ajout de la fonction goToConnexionAlerte qui affiche le message de confirmation. Le lien bouton est fonctionnel avec connexion.class et Inscription.class Images utilisées avec la classe image

--- trunk/Projet_QUIZ/src/Accueil.java
+++ trunk/Projet_QUIZ/src/Accueil.java
@@ -62,7 +62,7 @@ public class Accueil extends JPanel implements MouseListener, MouseMotionListene
 		if(e.getX() >= 959 && e.getX() <= 1022 && e.getY() >= 1 && e.getY() <= 47){ // CO/DECO
 			System.out.print("CO/DECO");
 			selection = "decoreco";
-			fenetre.goToConnexion(selection); // on appel la fonction qui va changer de panel
+			fenetre.goToConnexionAlerte(selection); // on appel la fonction qui va changer de panel
 
 		}
 		
