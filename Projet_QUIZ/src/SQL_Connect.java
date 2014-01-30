cc082edc107c9bfb297cf2c096cf4e452b83edca
quentincurtet@gmail.com <quentincurtet@gmail.com@b4cca557-fefa-2db8-3ffa-aea4168dc914>
2014-01-30 11:21:00 +0000

    Essai de requete SQL

--- Projet_QUIZ/src/SQL_Connect.java
+++ Projet_QUIZ/src/SQL_Connect.java
@@ -5,17 +5,15 @@ public class SQL_Connect {
 
 	public SQL_Connect() {
 		// TODO Auto-generated constructor stub
+		
 	}
 	public static void tryConnect() {
+		Connection conn = null;
 		try {
             // Load the SQLServerDriver class, build the
-            // connection string, and get a connection
+            // connection string, and get a connection	
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            String connectionUrl = "jdbc:sqlserver://193.252.48.189\\SQLEXPRESS:1433;"
-                    + "database=BDD_B3I_groupe_5;"
-                    + "user=b3i_groupe_5;"
-                    + "password=123Soleil";
-            Connection conn = DriverManager.getConnection(connectionUrl);
+            conn = DriverManager.getConnection("jdbc:sqlserver://193.252.48.189\\SQLEXPRESS:1433;" + "database=BDD_B3I_groupe_5;" + "user=b3i_groupe_5;" + "password=123Soleil");
             System.out.println("Connecté");
 		} 
 		catch (Exception e) {
