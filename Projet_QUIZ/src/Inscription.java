8e37919c6b2a3006e2a9f0e92d5a071a5212cab0
mickael.sinardet@gmail.com <mickael.sinardet@gmail.com@b4cca557-fefa-2db8-3ffa-aea4168dc914>
2014-01-30 11:22:15 +0000

    maj Inscription

--- Projet_QUIZ/src/Inscription.java
+++ Projet_QUIZ/src/Inscription.java
@@ -24,7 +24,19 @@ public class Inscription extends JPanel implements MouseListener, MouseMotionLis
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		
+		g.drawImage(Images.img_fond[0], 0, 0, this.getWidth(), this.getHeight(), null);
+		g.drawImage(Images.img_element[0], 0, 0, this.getWidth(), (int)(this.getHeight() / 6.1230), null);		// dessine le header		
+		/*
+		 * On boucle sur tous les boutons (de [0] a [3]).
+		 * Chaque bouton obtient une position X (hauteur) en fonction de son numero (i).
+		 * Les positions en durs (ici 120 et 260) correspondent au coin haut-gauche du bloc de bouton.
+		 */
+		for (int i=0; i<4; i++) {
+			g.drawImage(Images.img_bouton[i], 120, 260+(i*(Images.hauteur_bouton+Images.ecart_bouton)), Images.largeur_bouton, Images.hauteur_bouton, null);
+			g.drawImage(Images.img_bouton[i], 120, 260+(i*(Images.hauteur_bouton+Images.ecart_bouton)), Images.largeur_bouton, Images.hauteur_bouton, null);
+			g.drawImage(Images.img_bouton[i], 120, 260+(i*(Images.hauteur_bouton+Images.ecart_bouton)), Images.largeur_bouton, Images.hauteur_bouton, null);
+			g.drawImage(Images.img_bouton[i], 120, 260+(i*(Images.hauteur_bouton+Images.ecart_bouton)), Images.largeur_bouton, Images.hauteur_bouton, null);
+		}
 		g.drawImage(Images.img_fond[2], 0, 0, this.getWidth(), this.getHeight(), null);	
 	}
 	
