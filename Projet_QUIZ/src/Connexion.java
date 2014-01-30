7818c671754987d8669837d66fea9ee9d6209476
meroveecharrue@gmail.com <meroveecharrue@gmail.com@b4cca557-fefa-2db8-3ffa-aea4168dc914>
2014-01-30 10:44:41 +0000

    mini commit de page connexion

--- Projet_QUIZ/src/Connexion.java
+++ Projet_QUIZ/src/Connexion.java
@@ -1,12 +1,20 @@
+import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
 import javax.swing.JPanel;
 
-
+/**
+ * Class de connexion, s'affiche en tout debut de programme en pop-up style.
+ * Invite l'utilisateur a se connecter avant toute action.
+ * @author Merovee
+ */
 public class Connexion extends JPanel implements MouseListener, MouseMotionListener {
 	
+	/**
+	 * Implement les mehtod pour MouseListener et MouseMotionListener.
+	 */
 	public void mouseClicked(MouseEvent e) {}
 	public void mousePressed(MouseEvent e) {}
 	public void mouseReleased(MouseEvent e) {}
@@ -14,5 +22,14 @@ public class Connexion extends JPanel implements MouseListener, MouseMotionListe
 	public void mouseExited(MouseEvent e) {}
 	public void mouseDragged(MouseEvent e) {}
 	public void mouseMoved(MouseEvent e) {}
+	
+	/**
+	 * PAINT
+	 */
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		g.drawImage(Images.img_fond[1], 0, 0, this.getWidth(), this.getHeight(), null);	
+	}
 	
 }
