9e9776aad71eb550f8f324e128360d702b6ace9a
meroveecharrue@gmail.com <meroveecharrue@gmail.com@b4cca557-fefa-2db8-3ffa-aea4168dc914>
2014-01-30 10:36:18 +0000

    update page Connexion

--- Projet_QUIZ/src/Connexion.java
+++ Projet_QUIZ/src/Connexion.java
@@ -1,4 +1,18 @@
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
-public class Connexion {
+import javax.swing.JPanel;
+
+
+public class Connexion extends JPanel implements MouseListener, MouseMotionListener {
+	
+	public void mouseClicked(MouseEvent e) {}
+	public void mousePressed(MouseEvent e) {}
+	public void mouseReleased(MouseEvent e) {}
+	public void mouseEntered(MouseEvent e) {}
+	public void mouseExited(MouseEvent e) {}
+	public void mouseDragged(MouseEvent e) {}
+	public void mouseMoved(MouseEvent e) {}
 	
 }
