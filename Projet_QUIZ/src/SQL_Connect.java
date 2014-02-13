13e6fb40378ca5decad66bad8f19247160104f1e
quentincurtet@gmail.com <quentincurtet@gmail.com@b4cca557-fefa-2db8-3ffa-aea4168dc914>
2014-02-13 12:47:12 +0000

    Modif connexion BDD qui affiche l'accueil meme si on est pas co a la bdd

--- Projet_QUIZ/src/SQL_Connect.java
+++ Projet_QUIZ/src/SQL_Connect.java
@@ -1,13 +1,22 @@
 import java.sql.*;
 
+import javax.swing.JPanel;
 
-public class SQL_Connect {
 
-	public SQL_Connect() {
-		
+
+public class SQL_Connect extends JPanel{
+
+	private Fenetre fenetre;
+	static String selection; 
+	
+	public SQL_Connect(Fenetre fen) {
+		fenetre = fen;
+		fenetre.goToAccueil(selection);
 	}
+	
 	public static void tryConnect() {
 		Connection conn = null;
+		
 		try {	
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             conn = DriverManager.getConnection("jdbc:sqlserver://193.252.48.189\\SQLEXPRESS:1433;" + "database=BDD_B3I_groupe_5;" + "user=b3i_groupe_5;" + "password=123Soleil");
@@ -15,8 +24,9 @@ public class SQL_Connect {
 		} 
 		catch (Exception e) {
             System.out.println(e.getMessage());
-            System.out.println("erreur connection");
-            System.exit(0);
+            System.out.println("erreur connexion");
+            
 		}
+		
 	}
 }
