b88c7b0973cfea3cea6a968ee729bb7f2f2a49de
meroveecharrue@gmail.com <meroveecharrue@gmail.com@b4cca557-fefa-2db8-3ffa-aea4168dc914>
2014-02-19 13:40:43 +0000

    oubli: ajout_question

---
+++ Projet_QUIZ/src/Admin_ajout_questions.java
@@ -0,0 +1,37 @@
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JPanel;
+
+
+public class Admin_ajout_questions extends JPanel implements MouseListener {
+	
+	private Fenetre fenetre;
+	
+	public Admin_ajout_questions(Fenetre fen) {
+		fenetre = fen;
+		setBackground(Color.BLUE);
+	}
+	
+	@Override
+	public void mouseClicked(MouseEvent e) {
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+	}
+	
+}
