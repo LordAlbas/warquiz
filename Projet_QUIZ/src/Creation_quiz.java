7a365a3a0cdeac8f0ee6d8232f79b62144c90c82
meroveecharrue@gmail.com <meroveecharrue@gmail.com@b4cca557-fefa-2db8-3ffa-aea4168dc914>
2014-02-20 10:07:06 +0000

    Modif Creation_quiz

--- Projet_QUIZ/src/Creation_quiz.java
+++ Projet_QUIZ/src/Creation_quiz.java
@@ -27,6 +27,11 @@ public class Creation_quiz extends JPanel implements MouseListener, MouseMotionL
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTextField textField_2;
+	private String selection;
+	private String bouton_deco ="rien";
+	private String bouton_retour ="rien";
+	private String image_select = "rien";
+	private int val_i;
 	
 	public Creation_quiz(Fenetre fen){
 		setLayout(null);
@@ -100,8 +105,34 @@ public class Creation_quiz extends JPanel implements MouseListener, MouseMotionL
 	}
 	
 	public void mouseDragged(MouseEvent arg0) {}
-	public void mouseMoved(MouseEvent arg0) {}
-	public void mouseClicked(MouseEvent arg0) {}
+	public void mouseMoved(MouseEvent e) {
+		if(e.getX() >= 120 && e.getX() <= 490 && e.getY() >= 560 && e.getY() <= 645){ // QUITTER
+			//System.out.print("QUITTER_hover");
+			image_select = "QUITTER_hover";
+			val_i = 3;
+		}	
+		repaint(); // On re dessine
+	
+		if(e.getX() >= 959 && e.getX() <= 1022 && e.getY() >= 1 && e.getY() <= 47){ // CO/DECO
+			//System.out.print("CO/DECO_hover");
+			bouton_deco = "CO/DECO_hover";
+			//val_i = 4;
+			repaint(); // On re dessine
+		}	
+		if(e.getX() >= 1 && e.getX() <= 85 && e.getY() >= 685 && e.getY() <= 768){ // retour
+			bouton_retour = "retour_hover";
+			//val_i = 4;
+			repaint(); // On re dessine
+		}	
+	}
+	
+	public void mouseClicked(MouseEvent e) {
+		if(e.getX() >= 1 && e.getX() <= 85 && e.getY() >= 685 && e.getY() <= 768){ // Retour
+			System.out.print("Retour");
+			selection = "accueil";
+			fenetre.goToAccueil(selection); // on appel la fonction qui va changer de panel
+		}
+	}
 	public void mouseEntered(MouseEvent arg0) {}
 	public void mouseExited(MouseEvent arg0) {}
 	public void mousePressed(MouseEvent arg0) {}
@@ -112,5 +143,25 @@ public class Creation_quiz extends JPanel implements MouseListener, MouseMotionL
 		g.drawImage(Images.img_fond[0], 0, 0, this.getWidth(), this.getHeight(), null);
 		g.drawImage(Images.img_element[0], 0, 0, this.getWidth(), (int)(this.getHeight() / 6.1230), null);		// dessine le header	
 		g.drawImage(Images.img_bouton[4], 960, 1, 46, 46, null);
+		
+		switch (bouton_deco){
+		case "rien" :
+			g.drawImage(Images.img_bouton[4], 960, 1, 46, 46, null);
+			break;				
+		case "CO/DECO_hover" :
+			g.drawImage(Images.img_bouton_hover[4], 960, 1, 46, 46, null);
+			bouton_deco = "rien";
+			break;
+		}
+		
+		switch (bouton_retour)	{
+		case "rien" :
+			g.drawImage(Images.img_bouton[5], 1, 685, 84, 83, null);
+			break;				
+		case "retour_hover" :
+			g.drawImage(Images.img_bouton_hover[5], 1, 685, 84, 83, null);
+			bouton_retour = "rien";
+			break;
+		}
 	}
 }
