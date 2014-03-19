921e08f4bbe312e93e50d7639259cc7f232c2b91
meroveecharrue@gmail.com <meroveecharrue@gmail.com@b4cca557-fefa-2db8-3ffa-aea4168dc914>
2014-03-19 14:20:35 +0000

    epuration gningningnin Bouton_Ajout_reponse

--- trunk/Projet_QUIZ/src/Bouton_ajout_reponse.java
+++ trunk/Projet_QUIZ/src/Bouton_ajout_reponse.java
@@ -18,39 +18,19 @@ public class Bouton_ajout_reponse extends JButton implements MouseListener {
 		addMouseListener(this);
 		list_reponses = list_rep;
 	}
-	@Override
+	
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
 		if(list_reponses.getItemCount() < 10 && Admin_ajout_reponses.reponse.getText().length() != 0){
 			list_reponses.add(Admin_ajout_reponses.reponse.getText());
 			Admin_ajout_reponses.reponse.setText("");
 		} else {
-			System.out.println("ERRUER LIST PLEINE");
+			System.out.println("ERREUR LIST PLEINE");
 		}
 	}
 
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
+	public void mouseEntered(MouseEvent e) {}
+	public void mouseExited(MouseEvent e) {}
+	public void mousePressed(MouseEvent e) {}
+	public void mouseReleased(MouseEvent e) {}
 }
