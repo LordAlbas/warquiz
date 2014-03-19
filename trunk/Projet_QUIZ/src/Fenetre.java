f099bfc9b737240c425bf0ea6249edb30564f620
meroveecharrue@gmail.com <meroveecharrue@gmail.com@b4cca557-fefa-2db8-3ffa-aea4168dc914>
2014-03-19 14:05:55 +0000

    menage de la conchitaaa

--- trunk/Projet_QUIZ/src/Fenetre.java
+++ trunk/Projet_QUIZ/src/Fenetre.java
@@ -2,7 +2,6 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 
 import javax.swing.JFrame;
-import javax.swing.JLayeredPane;
 import javax.swing.JOptionPane;
 
 
@@ -10,15 +9,15 @@ public class Fenetre extends JFrame {
 	public static String titreFenetre = "(> o _ o )>          --[__W  A  R___Q  U  I  Z__]--          <[ x _ x <]";		// titre de la fenetre
 	public static Dimension tailleFenetre = new Dimension(1030, 796);	// taille de la fenetre
 
-	Statistiques statistiques ;
-	Accueil accueil;
-	Credits credits;
-	Jouer jouer;
-	Inscription inscription;
-	Connexion connexion;
-	Admin_ajout_reponses admin_ajout_reponses;
-	Creation_quiz creation_quiz;
-	private JLayeredPane jlp = new JLayeredPane();
+	// Definition des JPanel de l'appli (en private pour le moment)
+	public Statistiques statistiques ;
+	public Accueil accueil;
+	public Credits credits;
+	public Jouer jouer;
+	public Inscription inscription;
+	public Connexion connexion;
+	public Creation_quiz creation_quiz;
+
 	/**
 	 * Constructeur
 	 */
@@ -28,28 +27,25 @@ public class Fenetre extends JFrame {
 		setResizable(false);
 		setLocationRelativeTo(null);			// la fenetre apparait au milieu de l'ecran
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Images.initImage();
-		//Images img = new Images();
+		Images.initImage();		
 		
-		
+		// initialisation des JPanel
 		accueil = new Accueil(this);			// creation du JPanel accueil
 		statistiques = new Statistiques(this); // creation du JPanel statistiques
 		credits = new Credits(this);        	// creation du JPanel credits
 		jouer = new Jouer(this);				// creation du JPanel jouer
 		inscription = new Inscription(this);	// creation du JPanel inscription
 		connexion = new Connexion(this);			// creation du JPanel conexion
-		//admin_ajout_reponses = new Admin_ajout_reponses(this);
 		creation_quiz = new Creation_quiz(this);
 		
-		
-		
+		// ajout des mouseListener
 		accueil.addMouseListener(accueil);		// 'accueil' implemente les methodes relatif a l'ecoute de la souris
 		accueil.addMouseMotionListener(accueil);	
-		connexion.addMouseListener(connexion);		// 'accueil' implemente les methodes relatif a l'ecoute de la souris
+		connexion.addMouseListener(connexion);		// 'connexion' implemente les methodes relatif a l'ecoute de la souris
 		connexion.addMouseMotionListener(connexion);
 		inscription.addMouseListener(inscription);	// 'inscription' implemente les methodes relatif a l'ecoute de la souris
 		inscription.addMouseMotionListener(inscription);
-		creation_quiz.addMouseListener(creation_quiz);	// 'inscription' implemente les methodes relatif a l'ecoute de la souris
+		creation_quiz.addMouseListener(creation_quiz);	// 'creation_quiz' implemente les methodes relatif a l'ecoute de la souris
 		//creation_quiz.addMouseMotionListener(creation_quiz);
 		
 		setLayout(new GridLayout(1, 1));		// Layout grid (tableau)  1 colonne 1 ligne
@@ -66,7 +62,7 @@ public class Fenetre extends JFrame {
 	 * Redirige sur INSCRIPTION
 	 * @param selection
 	 */
-	public void goToInscription(String selection){
+	public void goToInscription(String selection) {
 		this.getContentPane().setVisible(false);
 		this.setContentPane(inscription);				// ajout du JPanel au JFrame (gridLayout)		
 		this.getContentPane().setVisible(true);
@@ -76,51 +72,32 @@ public class Fenetre extends JFrame {
 	 * Redirige sur CONNEXION SANS AFFICHER D'ALERTE
 	 * @param selection
 	 */
-	public void goToConnexion(String selection){
+	public void goToConnexion(String selection) {
 		this.getContentPane().setVisible(false);
-		this.setContentPane(connexion);				// ajout du JPanel au JFrame (gridLayout)		
+		this.setContentPane(connexion);		
 		this.getContentPane().setVisible(true);
-		//jlp.add(accueil, new Integer(0));
+	}
 	
-		//jlp.setPreferredSize(new Dimension (200, 200));
-		
-	
-		
-		//this.add(jlp);
-		//jlp.setVisible(true);
-		//jlp.add(connexion, new Integer (1));
-		//jlp.moveToFront(connexion);
-		//jlp.setOpaque(false);
-		
-		
-	}	
 	/**
 	 * Redirige sur CONNEXION
 	 * @param selection
 	 */
-	public void goToConnexionAlerte(String selection){
-		int rep_deco;
-		 rep_deco = JOptionPane.showConfirmDialog(null, "Voulez-vous vraiment vous déconnecter ?");
-
+	public void goToConnexionAlerte(String selection) {
+		int rep_deco = JOptionPane.showConfirmDialog(null, "Voulez-vous vraiment vous dï¿½connecter ?");
 		if(rep_deco == 0){
 			this.getContentPane().setVisible(false);	
-			this.setContentPane(connexion);				// ajout du JPanel au JFrame (gridLayout)
-
+			this.setContentPane(connexion);
 			this.getContentPane().setVisible(true);
-			//jlp.add(accueil, new Integer(0));
-			//jlp.add(connexion, new Integer(1));
-			//jlp.setVisible(true);
 		}
-	}	
-	/**
+	}
 	
 	/**
 	 * Redirige sur l'ACCUEIL
 	 * @param selection
 	 */
-	public void goToAccueil(String selection){
+	public void goToAccueil(String selection) {
 		this.getContentPane().setVisible(false);
-		this.setContentPane(accueil);				// ajout du JPanel au JFrame (gridLayout)		
+		this.setContentPane(accueil);
 		this.getContentPane().setVisible(true);
 	}
 	
@@ -128,11 +105,11 @@ public class Fenetre extends JFrame {
 	 * Redirige sur les STATISTIQUES
 	 * @param selection
 	 */
-	public void goToStatistiques(String selection){
+	public void goToStatistiques(String selection) {
 		this.getContentPane().setVisible(false);
 		statistiques.addMouseListener(statistiques);	// 'statistiques' implemente les methodes relatif a l'ecoute de la souris
 		statistiques.addMouseMotionListener(statistiques);
-		this.setContentPane(statistiques);				// ajout du JPanel au JFrame (gridLayout)		
+		this.setContentPane(statistiques);		
 		this.getContentPane().setVisible(true);
 	}
 	
@@ -140,11 +117,11 @@ public class Fenetre extends JFrame {
 	 * Redirige sur les CREDITS
 	 * @param selection
 	 */
-	public void goToCredits(String selection){
+	public void goToCredits(String selection) {
 		this.getContentPane().setVisible(false);
-		credits.addMouseListener(credits);	// 'statistiques' implemente les methodes relatif a l'ecoute de la souris
+		credits.addMouseListener(credits);				// 'statistiques' implemente les methodes relatif a l'ecoute de la souris
 		credits.addMouseMotionListener(credits);
-		this.setContentPane(credits);				// ajout du JPanel au JFrame (gridLayout)		
+		this.setContentPane(credits);
 		this.getContentPane().setVisible(true);
 	}
 	
@@ -152,11 +129,11 @@ public class Fenetre extends JFrame {
 	 * Redirige sur JOUER
 	 * @param selection
 	 */
-	public void goToJouer(String selection){
+	public void goToJouer(String selection) {
 		this.getContentPane().setVisible(false);
 		creation_quiz.addMouseListener(creation_quiz);	// 'statistiques' implemente les methodes relatif a l'ecoute de la souris
 		//creation_quiz.addMouseMotionListener(creation_quiz);
-		this.setContentPane(creation_quiz);				// ajout du JPanel au JFrame (gridLayout)		
+		this.setContentPane(creation_quiz);
 		this.getContentPane().setVisible(true);
 	}
 	
@@ -164,14 +141,11 @@ public class Fenetre extends JFrame {
 	 * QUITTE l'application
 	 * @param selection
 	 */
-	public void goToQuitter(String selection){
-		int rep;
-		 rep = JOptionPane.showConfirmDialog(null, "Voulez-vous vraiment quitter ?");
-
+	public void goToQuitter(String selection) {
+		int rep = JOptionPane.showConfirmDialog(null, "Voulez-vous vraiment quitter ?");
 		if(rep == 0){
-		 this.dispose();
+			this.dispose();
 		}
-		
 	}
 	
 	/**
@@ -180,8 +154,6 @@ public class Fenetre extends JFrame {
 	 */
 	public static void main(String[] args) {
 		Fenetre fenetre = new Fenetre();
-		
 		// et roule ma poule
 	}
-	
 }
