63700fc31e491823bcbeb630d729f5d9333fe11a
mickael.sinardet@gmail.com <mickael.sinardet@gmail.com@b4cca557-fefa-2db8-3ffa-aea4168dc914>
2014-04-09 13:41:48 +0000

    ajout de class tableau et amélioration de Bouton.java

--- trunk/Projet_QUIZ/src/Bouton.java
+++ trunk/Projet_QUIZ/src/Bouton.java
@@ -8,12 +8,14 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 import javax.swing.AbstractButton;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTable;
 
 
 public class Bouton extends JButton implements MouseListener{
@@ -25,9 +27,16 @@ public class Bouton extends JButton implements MouseListener{
 	private String db_name_quiz_diff;
 	private String db_nbquestion_quiz_diff;
 	private String txt;
-	
+	private JTable Tableau;
+	private Object[][] data;
+	private String[] columnNames;
+	private ArrayList<String[]> Tab = new ArrayList<String[]>();
 	public Bouton (String texte){
 
+		
+		String[] columnNames = {"Nom du Quiz", "Nombre de questions"}; // création des titres.
+		//Object[][] data = {{"Quiz 1", "32"}};
+		//Tableau = new JTable(data, columnNames);
 		txt = texte;
 		setLayout(null);
 		addMouseListener(this);
@@ -76,14 +85,25 @@ public class Bouton extends JButton implements MouseListener{
 	        	db_name_quiz_diff = rs_quiz_diff.getString("NOM_QUIZ"); // on récupère le score
 	        	db_nbquestion_quiz_diff = rs_quiz_diff.getString("NB_QUESTION"); // on récupère le num quiz
 	        	//db_diff = rs_score_diff.getString("DIFFICULTE_QUIZ"); // on récupère la difficulte
+	        	quiz_diff = db_name_quiz_diff + " : " + db_nbquestion_quiz_diff + " questions";       	
 	        	
-	        	quiz_diff = db_name_quiz_diff + " : " + db_nbquestion_quiz_diff + " questions";
-	        	//quiz_diff.setBounds(48, cpt_diff, 600,600);
+	        	
+	        	String[] inTab = {db_name_quiz_diff, db_nbquestion_quiz_diff};
+	        	Tab.add(inTab);
+
+	        	
 	        	System.out.println(quiz_diff);
-	        	//quiz_diff.setForeground(Color.WHITE); 
-	    		//add(quiz_diff);
-	    		//cpt_diff = cpt_diff +20;
 	        }
+	        
+	        Object[] data = Tab.toArray();
+	        Object[][] data2 = new Object[data.length][2];
+	        for (int i=0;i<data.length;i++) {
+	        	data2[i][0] = ((String[])data[i])[0];
+	        	data2[i][1] = ((String[])data[i])[1];
+	        }
+	        
+	        Tableau = new JTable(data2, columnNames);
+	        
 		} catch(ClassNotFoundException ee){
 			ee.printStackTrace();
 		} catch (SQLException e1) {
