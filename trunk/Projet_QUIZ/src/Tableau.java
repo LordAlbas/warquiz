68dea950eac6170205b7a71272988a678e238492
mickael.sinardet@gmail.com <mickael.sinardet@gmail.com@b4cca557-fefa-2db8-3ffa-aea4168dc914>
2014-04-16 12:07:08 +0000

    Ajout de fonctions dans classe bouton + modif class tableau

--- trunk/Projet_QUIZ/src/Tableau.java
+++ trunk/Projet_QUIZ/src/Tableau.java
@@ -12,15 +12,19 @@ public class Tableau extends JTable{
 	    this.donnees = donnees; 
 	    this.titres = titres; 
 	 } 
+	 
 	 public int getColumnCount(){ 
 	    return donnees[0].length; 
 	 }
+	 
 	 public Object getValueAt(int parm1, int parm2){ 
 	    return donnees[parm1][parm2]; 
 	 }
+	 
 	 public int getRowCount() { 
 	    return donnees.length; 
 	 }
+	 
 	 public String getColumnName(int col){ 
 	    return titres[col]; 
 	 } 
