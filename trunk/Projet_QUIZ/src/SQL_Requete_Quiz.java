064ad005f5b05a0f85abf0568c85d1d339d132b5
quentincurtet@gmail.com <quentincurtet@gmail.com@b4cca557-fefa-2db8-3ffa-aea4168dc914>
2014-04-24 09:57:44 +0000

    Modif classe requete quiz

--- trunk/Projet_QUIZ/src/SQL_Requete_Quiz.java
+++ trunk/Projet_QUIZ/src/SQL_Requete_Quiz.java
@@ -7,16 +7,36 @@ import java.sql.Statement;
 
 public class SQL_Requete_Quiz {
 	private Fenetre fenetre;
-	private String selection;
 	public String query_quiz; //variable dans lesquelles sont placées les requetes
+	public String query_nbQuiz;
 	private Quiz[] mesQuiz;
 	
 	public SQL_Requete_Quiz(Fenetre fen) {
 		fenetre = fen;
-		fenetre.goToAccueil(selection);
 	}
 	
-	
+	public int recup_nbQuiz(){
+		Connection conn = null;
+		int Nb_quiz = 0;
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			conn = DriverManager.getConnection("jdbc:sqlserver://193.252.48.189\\SQLEXPRESS:1433;" + "database=BDD_B3I_groupe_5;" + "user=b3i_groupe_5;" + "password=123Soleil");
+			Statement stmt_nbQuiz = (Statement) conn.createStatement();
+			query_nbQuiz = "SELECT COUNT(DISTINCT nom_quiz) AS nbQuiz FROM QUIZ";
+			stmt_nbQuiz.executeQuery(query_nbQuiz);
+	        
+	        ResultSet rs_nbQuiz = stmt_nbQuiz.getResultSet();
+	        
+	            while(rs_nbQuiz.next()){
+	            	Nb_quiz = rs_nbQuiz.getInt("nbQuiz");
+	            }
+	    } catch (SQLException eeee) {
+	    	eeee.printStackTrace();
+	    } catch (ClassNotFoundException eeee) {
+			eeee.printStackTrace();
+		}
+		return Nb_quiz;
+	}
 	
 	
 	public void recup_Quiz(){
@@ -29,18 +49,24 @@ public class SQL_Requete_Quiz {
 			stmt_quiz.executeQuery(query_quiz);
 	        
 	        ResultSet rs_quiz = stmt_quiz.getResultSet();
-	        
+	        mesQuiz = new Quiz[recup_nbQuiz()];
+	        short i = 0;
 	            while(rs_quiz.next()){
 	            	String Nom_quiz = rs_quiz.getString("nom_quiz");
 	            	int Id_quiz = rs_quiz.getInt("id_quiz");
-	            	Quiz monQuiz = new Quiz(Nom_quiz, fenetre);
-	            	monQuiz.setId(Id_quiz);
+	            	mesQuiz[i] = new Quiz(Nom_quiz, fenetre);
+	            	mesQuiz[i].setId(Id_quiz);
+	            	i++;
 	            }
 	    } catch (SQLException eeee) {
 	    	eeee.printStackTrace();
 	    } catch (ClassNotFoundException eeee) {
 			eeee.printStackTrace();
 		}		
+	}
+	
+	public Quiz[] getMesQuiz(){
+		return mesQuiz;	
 	}
 
 }
