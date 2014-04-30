24edc2c40526096a0d2d51b2f7516dd76f827686
mickael.sinardet@gmail.com <mickael.sinardet@gmail.com@b4cca557-fefa-2db8-3ffa-aea4168dc914>
2014-04-30 15:58:28 +0000

    Ajout du chronometre dans la partie_quiz

--- Projet_QUIZ/src/Jouer_partie.java
+++ Projet_QUIZ/src/Jouer_partie.java
@@ -1,10 +1,17 @@
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.util.TimerTask;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-
+import javax.swing.Timer;
+import javax.swing.*;
 
 public class Jouer_partie extends JPanel implements MouseListener, MouseMotionListener {
 	private Fenetre fenetre;
@@ -15,16 +22,24 @@ public class Jouer_partie extends JPanel implements MouseListener, MouseMotionLi
 	private String bouton_deco ="rien";
 	private String bouton_retour ="rien";
 	private String image_select;
-		
+	private JLabel lb_titrePartie;
 	private Header header1;
 	private Header_menu header2;
-	
+	private Timer timer;
+	private JLabel Chronometre;
+	private Chronometre chrono;
+	private int tempsInitial;
+	private int tempsRestant;
 	public Jouer_partie(Fenetre fen, Quiz quiz) {
 		fenetre = fen;
 		current_quiz = quiz;
 		setLayout(null);
 		
-		
+		lb_titrePartie = new JLabel("Partie : "+quiz);
+		lb_titrePartie.setForeground(Color.WHITE);
+		lb_titrePartie.setFont(new Font("Arial", Font.PLAIN, 42));
+		lb_titrePartie.setBounds(575, 105, 400, 50);
+		add(lb_titrePartie);
 		
 		//****Inclusion du Header en 2 parties ****
         header1 = new Header(fen);
@@ -40,36 +55,66 @@ public class Jouer_partie extends JPanel implements MouseListener, MouseMotionLi
         header2.addMouseMotionListener(header2);
         this.add(header2); 
         //****************************************
+        
+        
+        chrono = new Chronometre (0, 1, 12);
+        
+        
+        System.out.println(chrono.getTpsRestant());
+        Chronometre = new JLabel(chrono.getTpsRestant());
+        Chronometre.setForeground(Color.BLACK);
+        Chronometre.setOpaque(true);
+        Chronometre.setHorizontalAlignment(JLabel.CENTER);
+        Chronometre.setFont(new Font("Arial", Font.PLAIN, 20));
+        Chronometre.setBackground(Color.GREEN);
+        Chronometre.setBounds(850, 732, 174, 36);
+        add(Chronometre);
+        timer = createTimer();
+        timer.start();
+        
+        
 	}
-
-	@Override
-	public void mouseMoved(MouseEvent e) {
-		if(e.getX() >= 120 && e.getX() <= 490 && e.getY() >= 560 && e.getY() <= 645){ // QUITTER
-			//System.out.print("QUITTER_hover");
-			image_select = "QUITTER_hover";
-			//val_i = 3;
-		}
-		repaint(); // On re dessine
 	
-		if(e.getX() >= 959 && e.getX() <= 1022 && e.getY() >= 1 && e.getY() <= 47){ // CO/DECO
-			//System.out.print("CO/DECO_hover");
-			bouton_deco = "CO/DECO_hover";
-			//val_i = 4;
-			repaint(); // On re dessine
-		}
-		if(e.getX() >= 1 && e.getX() <= 85 && e.getY() >= 685 && e.getY() <= 768){ // retour
-			bouton_retour = "retour_hover";
-			//val_i = 4;
-			repaint(); // On re dessine
-		}
+	private Timer createTimer (){
+	    ActionListener action = new ActionListener (){
+	        // Méthode appelée à chaque tic du timer
+	        public void actionPerformed (ActionEvent event){
+	        	Chronometre.setText(chrono.getTpsRestant());
+	        	
+	        	
+	        	tempsInitial = (int)chrono.getChronoInital();
+	        	tempsRestant = (int)chrono.getChrono();
+	        	int case1 = tempsInitial-(tempsInitial/3);
+	        	int case2 = tempsInitial/3;
+	        	
+	        	if(tempsRestant > case1){
+	        		Chronometre.setForeground(Color.BLACK);
+	        		Chronometre.setBackground(Color.GREEN);
+	        	}
+	        	else if((tempsRestant <= case1) && tempsRestant > case2){
+	        		Chronometre.setForeground(Color.BLACK);
+	        		Chronometre.setBackground(Color.ORANGE);
+	        	}
+	        	else {
+	        		Chronometre.setForeground(Color.WHITE);
+	        		Chronometre.setBackground(Color.RED);
+	        	}
+	        	
+
+	        	
+	        	
+	        	Chronometre.repaint();
+	        }
+	      };
+	      
+	    // Création d'un timer qui génère un tic
+	    // chaque 1000 millième de seconde soit une seconde ! hé ouais maggle !
+	    return new Timer (1000, action);
 	}
-	public void mouseClicked(MouseEvent e) {
-		if(e.getX() >= 1 && e.getX() <= 85 && e.getY() >= 685 && e.getY() <= 768){ // Retour
-			System.out.print("Retour");
-			selection = "accueil";
-			fenetre.goToAccueil(selection); // on appel la fonction qui va changer de panel
-		}
-	}
+	
+	@Override
+	public void mouseMoved(MouseEvent e) {}
+	public void mouseClicked(MouseEvent e) {}
 	public void mousePressed(MouseEvent e) {}
 	public void mouseReleased(MouseEvent e) {}
 	public void mouseEntered(MouseEvent e) {}
@@ -82,24 +127,5 @@ public class Jouer_partie extends JPanel implements MouseListener, MouseMotionLi
 		//g.drawImage(Images.img_element[0], 0, 0, this.getWidth(), (int)(this.getHeight() / 6.1230), null);		// dessine le header	
 		g.drawImage(Images.img_bouton[4], 960, 1, 46, 46, null);
 		
-		switch (bouton_deco){
-		case "rien" :
-			g.drawImage(Images.img_bouton[4], 960, 1, 46, 46, null);
-			break;				
-		case "CO/DECO_hover" :
-			g.drawImage(Images.img_bouton_hover[4], 960, 1, 46, 46, null);
-			bouton_deco = "rien";
-			break;
-		}
-		
-		switch (bouton_retour)	{
-		case "rien" :
-			g.drawImage(Images.img_bouton[5], 1, 685, 84, 83, null);
-			break;				
-		case "retour_hover" :
-			g.drawImage(Images.img_bouton_hover[5], 1, 685, 84, 83, null);
-			bouton_retour = "rien";
-			break;
-		}
 	}
 }
