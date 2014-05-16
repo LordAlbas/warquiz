a33e79de82ef63cc0a6b80bfd91cae8109f7158f
quentincurtet@gmail.com <quentincurtet@gmail.com@b4cca557-fefa-2db8-3ffa-aea4168dc914>
2014-05-16 15:40:36 +0000

    Reglages timer

--- trunk/Projet_QUIZ/src/Quiz.java
+++ trunk/Projet_QUIZ/src/Quiz.java
@@ -15,8 +15,9 @@ public class Quiz {
 	private String nomQuiz;
 	private String difficulteQuiz;
 	private String categorieQuiz;
-	private int tempsQuiz = 5000;	// ceci est le temps limite du quiz
-	public Timer timerQuiz; // cela est juste l'objet timer qui permet de decompter jusqu'au tempsQuiz.
+	private int heureQuiz = 0;	
+	private int minuteQuiz = 0;
+	private int secondeQuiz = 0;// ceci est le temps limite du quiz
 	// pour lancer le timer, faire timerQuiz.start(), et il execute le 'actionPerformed' au bout de 'tempsQuiz' milisecond.
 	
 	/**
@@ -28,14 +29,6 @@ public class Quiz {
 		nomQuiz = nom;
 		nb_questions = 0;
 		difficulteQuiz = "default";
-		
-		timerQuiz = new Timer(tempsQuiz, new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				System.out.println("FIN DU TEMPS DE GAME !!!!");
-			}
-		});
-		timerQuiz.setRepeats(false);
-		// a partir d'ici, le timer est initialiser et pret a partir
 	}
 	
 	/**
@@ -73,8 +66,12 @@ public class Quiz {
 	public void setNb_questions(int nb) { nb_questions = nb; }
 	public int getNb_questions() { return nb_questions; }
 	// tempsQuiz
-	public void setTempsQuiz(int tmps) { tempsQuiz = tmps; }
-	public int getTempsQuiz() { return tempsQuiz; }
+	public void setHeureQuiz(int heure) { heureQuiz = heure; }
+	public int getHeureQuiz() { return heureQuiz; }
+	public void setMinuteQuiz(int tmps) { minuteQuiz = tmps; }
+	public int getMinuteQuiz() { return minuteQuiz; }
+	public void setSecondeQuiz(int tmps) { secondeQuiz = tmps; }
+	public int getSecondeQuiz() { return secondeQuiz; }
 	// questQuiz[i]
 	public Question getQuest(int i) { return questQuiz[i]; }
 	
