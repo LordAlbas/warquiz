b7b38eeffa2ae860665bd2ef114525c45ad8fd01
mickael.sinardet@gmail.com <mickael.sinardet@gmail.com@b4cca557-fefa-2db8-3ffa-aea4168dc914>
2014-05-17 13:25:08 +0000

    On ne peux plus revenir à l'accueil en cours de partie.

--- trunk/Projet_QUIZ/src/Header.java
+++ trunk/Projet_QUIZ/src/Header.java
@@ -4,6 +4,7 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 
@@ -13,24 +14,26 @@ public class Header extends JPanel implements MouseListener, MouseMotionListener
 	String bouton_option ="rien";
 	private Fenetre fenetre;
 	static String selection1;
-	private String warning;
-
+	private Boolean warning = true;
+	
 	public Header(Fenetre fen){
 		setLayout(null);
 		setOpaque(false);
 		fenetre = fen;
-		warning = "";
 		setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 	}
 
 	public void mouseClicked(MouseEvent e) {
 		
-		fenetre.goToAccueil(selection1);
 		
-		if (warning != "") {
-			// j'ai un message
+		
+		if (warning){
+			fenetre.goToAccueil(selection1);
 		} else {
-			// j'ai pas de message
+			JOptionPane.showMessageDialog(null,
+				    "Impossible de retourner à l'accueil en cours de partie !",
+				    "Erreur",
+				    JOptionPane.WARNING_MESSAGE);
 		}
 		
 		/*
@@ -50,48 +53,17 @@ public class Header extends JPanel implements MouseListener, MouseMotionListener
 
 	public void mouseDragged(MouseEvent arg0) {	}
 
-	public void mouseMoved(MouseEvent e) {
-		
-		if(e.getX() >= 905 && e.getX() <= 969 && e.getY() >= 1 && e.getY() <= 47){ // OPT
-			//System.out.print("OPT_hover");
-			bouton_option = "OPTION_hover";
-		}else{bouton_option = "rien";}		
-		
-		if(e.getX() >= 970 && e.getX() <= 1022 && e.getY() >= 1 && e.getY() <= 47){ // DECO
-			//System.out.print("DECO_hover");
-			bouton_deco = "DECO_hover";
-		}else{bouton_deco = "rien";}
-		repaint();
-	}
+	public void mouseMoved(MouseEvent e) {}
 	
-	public void setWarning(String msg) { warning = msg; }
-	public String getWarning() { return warning; }
+	public void setWarning(Boolean msg) { warning = msg; }
+	public Boolean getWarning() { return warning; }
 
     @Override
     protected void paintComponent(Graphics g){
         super.paintComponent(g);
         
         g.drawImage(Images.img_element[9], 0, 0, this.getWidth(), this.getHeight(), this);
-        /**
-		switch (bouton_deco){
-		case "rien" :
-			g.drawImage(Images.img_bouton[4], 970, 0, 46, 46, null);
-			break;				
-		case "DECO_hover" :
-			g.drawImage(Images.img_bouton_hover[4], 970, 0, 46, 46, null);
-			bouton_deco = "rien";
-			break;	
-		}
-		
-		switch (bouton_option){
-		case "rien" :
-			g.drawImage(Images.img_bouton[6], 905, 0, 46, 46, null);
-			break;				
-		case "OPTION_hover" :
-			g.drawImage(Images.img_bouton_hover[6], 905, 0, 46, 46, null);
-			bouton_option = "rien";
-			break;	
-		}	*/
+
     }
 
 }
